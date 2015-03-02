bb264f96ee8eb9e34295ac485aecdff6b22894d3
hilburn <charliepaterson89@gmail.com>
2015-03-02 18:54:14 +0000

    Copy update

--- src/main/java/stevesaddons/asm/StevesHooks.java
+++ src/main/java/stevesaddons/asm/StevesHooks.java
@@ -5,13 +5,14 @@ import stevesaddons.helpers.StevesEnum;
 import vswe.stevesfactory.Localization;
 import vswe.stevesfactory.blocks.TileEntityManager;
 import vswe.stevesfactory.components.ComponentType;
+import vswe.stevesfactory.components.Connection;
+import vswe.stevesfactory.components.ConnectionSet;
 import vswe.stevesfactory.components.FlowComponent;
 import vswe.stevesfactory.network.DataReader;
 import vswe.stevesfactory.network.DataWriter;
 import vswe.stevesfactory.settings.Settings;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class StevesHooks
 {
@@ -33,13 +34,8 @@ public class StevesHooks
                         if (!itr.hasNext()) return;
                         item = itr.next();
                     } while (item.getId() != id);
-                    FlowComponent newComponent = item.copy();
-                    newComponent.clearConnections();
-                    newComponent.resetPosition();
-                    newComponent.setX(50);
-                    newComponent.setY(50);
-                    newComponent.setId(manager.getFlowItems().size());
-                    manager.getFlowItems().add(newComponent);
+                    Collection<FlowComponent> added = copyConnectionsWithChildren(manager.getFlowItems(),item);
+                    manager.getFlowItems().addAll(added);
                 }
 
             }
@@ -70,7 +66,67 @@ public class StevesHooks
                 return !Settings.isLimitless(manager) && manager.getFlowItems().size() == 511 ? Localization.MAXIMUM_COMPONENT_ERROR.toString() : super.getMouseOver();
             }
         });
-        return;
+    }
+
+    private static Collection<FlowComponent> copyConnectionsWithChildren(List<FlowComponent> existing, FlowComponent toCopy)
+    {
+        Map<FlowComponent, FlowComponent> added = new LinkedHashMap<FlowComponent, FlowComponent>();
+        copyConnectionsWithChildren(added, existing, toCopy, toCopy.getParent(), true);
+        if (added.size() + existing.size() > 511)
+        {
+            Iterator<Map.Entry<FlowComponent, FlowComponent>> itr = added.entrySet().iterator();
+            for (int index = 0; itr.hasNext(); index++)
+            {
+                itr.next();
+                if (index > 511 - existing.size()) itr.remove();
+            }
+        }
+        reconnect(added);
+        return added.values();
+    }
+
+    private static void copyConnectionsWithChildren(Map<FlowComponent, FlowComponent> added, List<FlowComponent> existing, FlowComponent toCopy, FlowComponent newParent, boolean reset)
+    {
+        FlowComponent newComponent = toCopy.copy();
+        newComponent.clearConnections();
+        newComponent.setParent(newParent);
+        if (reset)
+        {
+            newComponent.resetPosition();
+            newComponent.setX(50);
+            newComponent.setY(50);
+        }
+        newComponent.setId(existing.size()+added.size());
+        added.put(toCopy, newComponent);
+        for (FlowComponent component : existing)
+        {
+            if (component.getParent() == toCopy)
+            {
+                copyConnectionsWithChildren(added, existing, component, newComponent, false);
+            }
+        }
+    }
+
+    private static void reconnect(Map<FlowComponent, FlowComponent> added)
+    {
+        Map<Integer, FlowComponent> oldComponents = new HashMap<Integer, FlowComponent>();
+        for (FlowComponent component : added.keySet())
+        {
+            oldComponents.put(component.getId(),component);
+        }
+
+        for (FlowComponent component : added.keySet())
+        {
+            for (Map.Entry<Integer, Connection> entry : component.getConnections().entrySet())
+            {
+                FlowComponent connectTo = added.get(oldComponents.get(entry.getValue().getComponentId()));
+                if (connectTo != null)
+                {
+                    Connection newConnection = new Connection(connectTo.getId(),entry.getValue().getConnectionId());
+                    added.get(component).setConnection(entry.getKey(),newConnection);
+                }
+            }
+        }
     }
 
     public static ItemStack fixLoadingStack(ItemStack stack)
